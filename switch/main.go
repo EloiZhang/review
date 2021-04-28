168dab04d81f11bbc77295b567072a7a889ab3b4
eloizhang <eloizhang@tencent.com>
2021-04-28 16:43:38 +0800

    优化for switch结构

--- switch/main.go
+++ switch/main.go
@@ -1,5 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	m := map[string]func(){
+		"1": func1,
+		"2": func2,
+	}
+	mReq := make(map[string]string)
+	mReq["1"] = "1"
+	mReq["2"] = "2"
+	for req := range mReq{
+		if f,ok := m[req];ok{
+			f()
+		}
+	}
+}
+func func1() {
+	fmt.Println("1")
+}
+func func2() {
+	fmt.Print("2")
 }
